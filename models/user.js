3ea6598add4cfa920d6471fd68b00502bb2942e4
Evisa Lumani <evisa.lumani@tum.de>
2016-11-02 23:59:07 +0100

    Add "validator" package; use isEmail validator in the user schema

--- models/user.js
+++ models/user.js
@@ -1,15 +1,18 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var mongooseUniqueValidator = require('mongoose-unique-validator');
+var validator = require('validator');
 
 var schema = new Schema({
     name: { type: String, required: true, minlength: 4},
     email: {
         type: String,
-        required: true, unique: true
+        required: true,
+        unique: true,
+        validate: [ validator.isEmail, 'Invalid email syntax' ]
     },
     password: { type: String, required: true },
-    personalRole: { type: String, required: true}
+    personalRole: { type: String, required: true }
 });
 
 schema.plugin(mongooseUniqueValidator);
