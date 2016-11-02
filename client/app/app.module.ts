49f1d86d40177e6dffe737eded43e509c9dbc49b
Evisa Lumani <evisa.lumani@tum.de>
2016-11-02 19:25:57 +0100

    Group footer and navbar components into a separate directory

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from "./app.component";
-import {NavbarComponent} from "./navbar.component";
-import {FooterComponent} from "./footer.component";
+import {NavbarComponent} from "./navigation/navbar.component";
+import {FooterComponent} from "./navigation/footer.component";
 
 @NgModule({
     declarations: [
