ec7fa25c054a27be61b46e831362bc0c3e320ccf
Evisa Lumani <evisa.lumani@tum.de>
2016-12-05 19:40:19 +0100

    Add some dummy data as reference

---
+++ themesDummyData.js
@@ -0,0 +1,34 @@
+var themes = [
+    {
+        name: 'Precision agriculture',
+        tags: ['agriculture', 'growing population', 'arable land', 'climate change', 'green'],
+        description: "Is the agricultural industry rising to the challenge of growing populations, higher-calorie diets, declining arable land\ " +
+        "and climate change? These factors place an urgent responsibility on the world’s food industry to raise its productivity.\ " +
+        "The outlook is challenging, yet there are encouraging signs that agriculture is undergoing a second ‘green revolution’\ " +
+        "as new technologies are introduced."
+    },
+    {
+        name: 'Information technology',
+        tags: ['information', 'technical revolution', 'disruptive technology'],
+        description: "We are in the midst of a period of rapid innovation known as ‘the information revolution’ – a third phase\ " +
+        "of great technical change after the transformative agricultural and industrial revolutions"
+    },
+    {
+        name: 'Wearable technology',
+        tags: ['devices', 'software', 'data management', 'data security', 'healthcare', 'media'],
+        description: "Wearable technology is set to alter many areas of life – from healthcare, to retail, media and communications. Businesses involved" +
+        " in the production of devices, software, data storage and security stand to benefit, as do non-tech businesses which can devise ways of leveraging the technology."
+    },
+    {
+        name: "Automobile industry",
+        tags: ['electric cars', 'self-driving cars'],
+        description: "Thanks in large part to the outstanding success of Tesla’s electric vehicles and ongoing innovations in terms of driverless cars, media interest in" +
+        " the so-called ‘car of the future’ continues to grow."
+    },
+    {
+        name: "Internet of Things",
+        tags: ['iot', 'embedded devices', 'sensors', 'cyber-physical systems', 'realtime systems'],
+        description: "The ‘Internet of Things’ (IoT) is the name given to the growing range of internet-connected objects with embedded sensors that can process and share\ " +
+        "complex information without human involvement."
+    }
+]
