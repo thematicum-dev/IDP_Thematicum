2be6be08cd45334a144f09d553b0bfbc4a3d98dc
Evisa Lumani <evisa.lumani@tum.de>
2016-12-20 00:05:06 +0100

    Remove unneeded code in theme creation component

--- client/app/theme_creation/theme-creation.component.ts
+++ client/app/theme_creation/theme-creation.component.ts
@@ -20,75 +20,24 @@ import {AutoCompleteContainerComponent} from "../autocomplete/autocomplete-conta
     templateUrl: 'theme-creation.component.html',
     styles: [`.btn-default.active, .btn-default:active {
         background-color: #d9edf7
-    }`],
-    directives: [AutoCompleteComponent]
+    }`]
 })
-export class ThemeCreationComponent implements OnInit {
-    selectedTags: string[] = [];
-    tagsPlaceholder = 'Keyword';
-    allowCustomValues: boolean = true;
-    allowEnterKey: boolean = true;
-    allowDirectClick: boolean = true;
-    error: string = '';
+export class ThemeCreationComponent {
     themeCreation: ThemeCreationModel;
-    tagList: AutocompleteItem[] = [];
-
-    ngOnInit(): void {
-        // console.log('ngInit in theme creation')
-        // this.themeTagService.getAutocompleteList().subscribe(data => {
-        //         for (let tag of data) {
-        //             this.tagList.push(new AutocompleteItem(tag));
-        //         }
-        //     },
-        //     error => {
-        //         console.log(error)
-        //     });
-    }
 
     constructor(
-        private elementRef: ElementRef,
         private themeService: ThemeService,
         private router: Router) {
 
         let theme = new Theme();
-        theme.tags = this.selectedTags;
+        // theme.tags = this.selectedTags;
         this.themeCreation = new ThemeCreationModel(theme, new ThemeProperties());
     }
-    //
-    // onNotifySelectedItem(tag: any) {
-    //     //search by name (assume unique name)
-    //     let existingItem = this.selectedTags.find(el => {
-    //         return el == tag.name
-    //     });
-    //
-    //     //do not add an item if it was already selected
-    //     if(!existingItem) {
-    //         this.selectedTags.push(tag.name);
-    //     } else {
-    //         this.error = 'This item has already been selected. Please choose another one';
-    //     }
-    // }
-
-    onNotifyAllocatedStocks(allocatedStock: StockAllocationModel) {
-        this.themeCreation.stockAllocation.push(allocatedStock);
-    }
-
-    // onTagDeselect(index: number) {
-    //     if (index >= 0 && index < this.selectedTags.length) {
-    //         this.selectedTags.splice(index, 1);
-    //     }
-    // }
-    //
-    // onClearErrorStr() {
-    //     this.error = '';
-    // }
 
     onSubmit(form: NgForm, themeTags: AutoCompleteContainerComponent, themeStockAllocation: AutoCompleteContainerComponent) {
         //call service to save theme
-        //this.setCheckedCategories();
         console.log(themeTags.selectedItems)
         console.log(themeStockAllocation.selectedItems)
-
 
         return;
         this.themeCreation.themeProperties.setCheckedCategories();
