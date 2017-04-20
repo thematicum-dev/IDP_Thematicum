3301be9ec46bf429fdea98b08985d30530fc6147
Sherjeel Sikander <sherjeel.337@gmail.com>
2017-04-20 20:45:13 +0200

    Fix Start Searching text to Sign in when user is not logged in

--- client/app/navigation/homepage.component.ts
+++ client/app/navigation/homepage.component.ts
@@ -1,8 +1,15 @@
 import {Component} from '@angular/core';
+import {AuthService} from "../auth/auth.service";
+
 @Component({
     selector: 'app-homepage',
     templateUrl: 'homepage.component.html'
 })
 export class HomepageComponent {
     title = "Thematic risk research";
+    constructor(private authService: AuthService) {}
+    
+    isLoggedIn() {
+        return this.authService.getStoredToken() != null;
+    }
 }
