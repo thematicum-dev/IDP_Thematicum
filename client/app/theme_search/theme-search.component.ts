e35e30fbf027507a3c12e15998b5bd33cdd759ad
Sherjeel Sikander <sherjeel.337@gmail.com>
2017-05-10 03:08:26 +0200

    Back and Forward button work on search page, Refactor text options

--- client/app/theme_search/theme-search.component.ts
+++ client/app/theme_search/theme-search.component.ts
@@ -1,54 +1,60 @@
 import {Component, OnInit} from '@angular/core';
 import {NgForm} from "@angular/forms";
 import {Theme} from "../models/theme";
-import {Router, ActivatedRoute} from "@angular/router";
+import {Router, ActivatedRoute, Params} from "@angular/router";
 import {ThemeService} from "../services/theme.service";
 import { IMultiSelectOption, IMultiSelectTexts, IMultiSelectSettings} from 'angular-2-dropdown-multiselect';
+import * as _ from 'underscore';
 
 @Component({
     selector: 'app-theme-search',
-    templateUrl: 'theme-search.component.html' 
+    templateUrl: 'theme-search.component.html'
 })
 export class ThemeSearchComponent implements OnInit {
     searchTerm = "";
     themes: Theme[] = [];
 
-    constructor(private themeService: ThemeService, private router: Router, private route: ActivatedRoute) {}
+    generalTextOptions: IMultiSelectTexts = {
+        checkAll: 'Select all',
+        uncheckAll: 'Unselect all',
+        checked: 'item selected',
+        checkedPlural: 'items selected',
+        searchPlaceholder: 'Search',
+        defaultTitle: '',
+        allSelected: 'All Selected'
+    };
+    categoryTextOptions = _.clone(this.generalTextOptions);
+    timeHorizonTextOptions = _.clone(this.generalTextOptions);
+    maturityTextOptions = _.clone(this.generalTextOptions);
+    tagTextOptions = _.clone(this.generalTextOptions);
 
-    ngOnInit(): void {
-        //TODO: refactor - create separate classes to override the 2 cases implementation
-        if(this.route.snapshot.queryParams['query']) {
-            this.searchTerm = this.route.snapshot.queryParams['query'].trim();
-            this.searchThemes(this.searchTerm, [], [], [], []);
-        } else if(this.route.snapshot.queryParams['all'] && this.route.snapshot.queryParams['all'] === 'true') {
-            this.searchThemes(null, [], [], [], []);
-        }
-        this.themeService.getAllThemeTags().subscribe(data => this.updateTagList(data), error => console.log(error));
-    }
+    categoryOptionsModel: number[] = [];
+    timeHorizonOptionsModel: number[] = [];
+    maturityOptionsModel: number[] = [];
+    tagOptionsModel: string[] = [];
 
-    onSubmit(form: NgForm) {
-        if(this.searchTerm) {
-            this.router.navigate([], {queryParams:{query: this.searchTerm}});
-            this.searchThemes(this.searchTerm, this.categoryOptionsModel, this.maturityOptionsModel, this.timeHorizonOptionsModel, this.tagOptionsModel);
-        } else {
-            this.router.navigate([], {queryParams:{all: 'true'}});
-            this.searchThemes(null, this.categoryOptionsModel, this.maturityOptionsModel, this.timeHorizonOptionsModel, this.tagOptionsModel);
-        }
-    }
+    categoryOptions: IMultiSelectOption[] = [
+        { id: 0, name: 'Economic' },
+        { id: 1, name: 'Technologic' },
+        { id: 2, name: 'Environmental' },
+        { id: 3, name: 'Political' },
+        { id: 4, name: 'Regulatory' },
+        { id: 5, name: 'Sociologic' }
+    ];
+    timeHorizonOptions: IMultiSelectOption[] = [
+        { id: 0, name: 'Short Term' },
+        { id: 1, name: 'Medium Term' },
+        { id: 2, name: 'Long Term' }
+    ];
+    maturityOptions: IMultiSelectOption[] = [
+        { id: 0, name: 'Upcoming' },
+        { id: 1, name: 'Nascent' },
+        { id: 2, name: 'Accelerating' },
+        { id: 3, name: 'Mature' },
+        { id: 4, name: 'Declining' }
+    ];
+    tagOptions: IMultiSelectOption[] = [];
 
-    goToThemeDetails(themeId: string) {
-        this.router.navigate(['/theme', themeId]);
-    }
-
-    updateView(data: any){
-        this.themes = data[1];
-    }
-
-    searchThemes(searchTerm: any, categoryOptionsModel: number[], maturityOptionsModel: number[], timeHorizonOptionsModel: number[], tagOptionsModel: string[]) {
-        this.themeService.searchThemes(searchTerm, categoryOptionsModel, maturityOptionsModel, timeHorizonOptionsModel, tagOptionsModel).subscribe(data => this.updateView(data), error => console.log(error));
-    }  
-
-    // TODO: Find a way to reuse code for settings and options
     noSearchDropdownSettings: IMultiSelectSettings = {
         enableSearch: false,
         checkedStyle: 'checkboxes',
@@ -57,7 +63,6 @@ export class ThemeSearchComponent implements OnInit {
         displayAllSelectedText: true,
         containerClasses: 'full-width'
     };
-
     searchDropdownSettings: IMultiSelectSettings = {
         enableSearch: true,
         checkedStyle: 'checkboxes',
@@ -66,75 +71,86 @@ export class ThemeSearchComponent implements OnInit {
         displayAllSelectedText: true,
         containerClasses: 'full-width'
     };
-   
-    categoryTextOptions: IMultiSelectTexts = {
-        checkAll: 'Select all',
-        uncheckAll: 'Unselect all',
-        checked: 'item selected',
-        checkedPlural: 'items selected',
-        searchPlaceholder: 'Search',
-        defaultTitle: 'Category',
-        allSelected: 'All Selected'
-    };
-    categoryOptionsModel: number[] = []; 
-    categoryOptions: IMultiSelectOption[] = [
-        { id: 0, name: 'Economic'},
-        { id: 1, name: 'Technologic' },
-        { id: 2, name: 'Environmental'},
-        { id: 3, name: 'Political'},
-        { id: 4, name: 'Regulatory'},
-        { id: 5, name: 'Sociologic'}
-    ];
 
+    constructor(private themeService: ThemeService, private router: Router, private route: ActivatedRoute) { }
 
-    timeHorizonOptionsModel: number[] = []; 
-    timeHorizonTextOptions: IMultiSelectTexts = {
-        checkAll: 'Select all',
-        uncheckAll: 'Unselect all',
-        checked: 'item selected',
-        checkedPlural: 'items selected',
-        searchPlaceholder: 'Search',
-        defaultTitle: 'Time Horizon',
-        allSelected: 'All Selected'
-    };
-    timeHorizonOptions: IMultiSelectOption[] = [
-        { id: 0, name: 'Short Term'},
-        { id: 1, name: 'Medium Term'},
-        { id: 2, name: 'Long Term'}
-    ];
+    ngOnInit(): void {
+        this.route.queryParams.subscribe((queryParams: Params) => {
+            // this method works with browser back and forward button
+            this.searchThemeOnRouteChange(queryParams);
+        });
+        this.categoryTextOptions.defaultTitle = 'Category';
+        this.timeHorizonTextOptions.defaultTitle = 'Time Horizon';
+        this.maturityTextOptions.defaultTitle = 'Maturity';
+        this.tagTextOptions.defaultTitle = 'Tags';
+        this.themeService.getAllThemeTags().subscribe(data => this.updateTagList(data), error => console.log(error));
+    }
 
-    maturityOptionsModel: number[] = [];
-    maturityTextOptions: IMultiSelectTexts = {
-        checkAll: 'Select all',
-        uncheckAll: 'Unselect all',
-        checked: 'item selected',
-        checkedPlural: 'items selected',
-        searchPlaceholder: 'Search',
-        defaultTitle: 'Maturity',
-        allSelected: 'All Selected'
-    }; 
-    maturityOptions: IMultiSelectOption[] = [
-        { id: 0, name: 'Upcoming'},
-        { id: 1, name: 'Nascent'},
-        { id: 2, name: 'Accelerating'},
-        { id: 3, name: 'Mature'},
-        { id: 4, name: 'Declining'}
-    ];
+    searchThemeOnRouteChange(queryParams) {
+        if (queryParams['query'])
+            this.searchTerm = queryParams['query'].trim()
+        else
+            this.searchTerm = '';
 
-    tagOptionsModel: string[] = []; 
-    tagOptions: IMultiSelectOption[] = [];
-    updateTagList(tags){
-        for(let tag of tags){
-            this.tagOptions.push({id: tag, name: tag});
+        var getParamNumberModel = function (param) {
+            if (queryParams[param])
+                if (Array.isArray(queryParams[param]))
+                    return queryParams[param].map(Number);
+                else
+                    return [parseInt(queryParams[param], 10)];
+            else
+                return [];
+        }
+
+        this.categoryOptionsModel = getParamNumberModel('categories');
+        this.timeHorizonOptionsModel = getParamNumberModel('timeHorizon');
+        this.maturityOptionsModel = getParamNumberModel('maturity');
+
+        var getParamStringModel = function (param) {
+            if (queryParams[param])
+                if (Array.isArray(queryParams[param]))
+                    return queryParams[param];
+                else
+                    return [queryParams[param]];
+            else
+                return [];
+        }
+
+        this.tagOptionsModel = getParamStringModel('tags');
+
+        if (!(Object.keys(queryParams).length === 0 && queryParams.constructor === Object))
+            this.searchThemes(this.searchTerm, this.categoryOptionsModel, this.maturityOptionsModel, this.timeHorizonOptionsModel, this.tagOptionsModel);
+    }
+
+    onSubmit(form: NgForm) {
+        // Calls searchThemeOnRouteChange
+        this.router.navigate([], {
+            queryParams: {
+                query: this.searchTerm,
+                categories: this.categoryOptionsModel,
+                timeHorizon: this.timeHorizonOptionsModel,
+                maturity: this.maturityOptionsModel,
+                tags: this.tagOptionsModel
+            }
+        });
+    }
+
+    goToThemeDetails(themeId: string) {
+        this.router.navigate(['/theme', themeId]);
+    }
+
+    updateView(data: any) {
+        this.themes = data[1];
+    }
+
+    searchThemes(searchTerm: any, categoryOptionsModel: number[], maturityOptionsModel: number[], timeHorizonOptionsModel: number[], tagOptionsModel: string[]) {
+        this.themeService.searchThemes(searchTerm, categoryOptionsModel, maturityOptionsModel, timeHorizonOptionsModel, tagOptionsModel).subscribe(data => this.updateView(data), error => console.log(error));
+    }
+
+    updateTagList(tags) {
+        for (let tag of tags) {
+            this.tagOptions.push({ id: tag, name: tag });
         }
     }
-    tagTextOptions: IMultiSelectTexts = {
-        checkAll: 'Select all',
-        uncheckAll: 'Unselect all',
-        checked: 'item selected',
-        checkedPlural: 'items selected',
-        searchPlaceholder: 'Search',
-        defaultTitle: 'Tags',
-        allSelected: 'All Selected'
-    }; 
+
 }
