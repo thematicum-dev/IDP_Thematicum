92bd352457758957ed9bedda52bcd52428b86016
Sherjeel Sikander <sherjeel.337@gmail.com>
2017-05-28 18:07:03 +0200

    New theme dummy data

--- server/utilities/themesDummyData.js
+++ server/utilities/themesDummyData.js
@@ -30,5 +30,55 @@ const themes = [
         tags: ['iot', 'embedded devices', 'sensors', 'cyber-physical systems', 'realtime systems'],
         description: "The ‘Internet of Things’ (IoT) is the name given to the growing range of internet-connected objects with embedded sensors that can process and share\ " +
         "complex information without human involvement."
+    },
+    {
+        name: "Aging Population",
+        tags: ['age', 'aging', 'population'],
+        description: "The needs and demands of the expanding, older population"
+    },
+    {
+        name: "Cashless Cosumption",
+        tags: ['cashless', 'online', 'shopping'],
+        description: "Scanning and swiping through online and mobile shopping coupled with the growing pervasiveness of smartphones."
+    },
+    {
+        name: "Connected Society",
+        tags: ['connected', 'mobility'],
+        description: "The intersection of computing, broadband, mobility and the Internet changing the way we live."
+    },
+    {
+        name: "Fattening Population",
+        tags: ['fat', 'population'],
+        description: "Capitalize on companies positioned to address the expanding waistline of the population."
+    },
+    {
+        name: "Foundation of Youth",
+        tags: ['youth', 'aging'],
+        description: "The products and services aging Baby Boomers seek to hold off the aging process."
+    },
+    {
+        name: "Rise fall middle class",
+        tags: ['middle', 'class', 'rise', 'fall'],
+        description: "The worldwide expanding new middle class spurs demand for goods and services that previously had not been available."
+    },
+    {
+        name: "Safety and Security",
+        tags: ['safety', 'security', 'defense'],
+        description: "Personal, corporate and homeland security, this theme revolves around increasingly on guard behaviours and need to shift from reactionary defense to always prepared and secure."
+    },
+    {
+        name: "Scarce Resources",
+        tags: ['resources', 'scarce'],
+        description: "Companies positioned to benefit from a scarce resource or those that could see their margins and earnings come under pressure as key input prices escalate."
+    },
+    {
+        name: "Tooling and Retooling",
+        tags: ['qualified', 'worker', 'employment', 'under-employment'],
+        description: "The crosshairs of high under-employment and hiring managers that can not find qualified workers."
+    },
+    {
+        name: "Ultra Wealthy",
+        tags: ['wealthy', 'services'],
+        description: "Those products and service that appeal to the expanding number of super wealthy throughout the world."
     }
 ];
