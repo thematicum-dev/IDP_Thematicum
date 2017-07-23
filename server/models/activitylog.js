392f62df83fce4ece3178b346dc0ed78b05fc1a9
taimoor <taimoor.alam12@gmail.com>
2017-07-23 12:29:00 +0200

    add user theme stock composition in activitylog model

--- server/models/activitylog.js
+++ server/models/activitylog.js
@@ -4,11 +4,13 @@ import constants from '../utilities/constants';
 const Schema = mongoose.Schema;
 const UserThemeInputSchema = mongoose.model('UserThemeInput').schema;
 const UserThemeStockAllocation = mongoose.model('UserThemeStockAllocation').schema;
+const UserThemeStockComposition = mongoose.model('ThemeStockComposition').schema;
 const schema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: 'User' },
     theme: { type: Schema.Types.ObjectId, ref: 'Theme', index: true },
     userThemeInput: { type: UserThemeInputSchema, default: null },
     userThemeStockAllocation: { type: UserThemeStockAllocation, default: null },
+    stock: {type: UserThemeStockComposition, default: null},
     createdAt : { type : Date, default: Date.now }
 });
 
