2eb5095edcc69f3a47ac93c6e175062ca2f2d371
Sherjeel Sikander <sherjeel.337@gmail.com>
2017-09-06 18:35:58 +0200

    add space to tags on theme detail page

--- client/app/theme_details/theme-details.component.ts
+++ client/app/theme_details/theme-details.component.ts
@@ -75,4 +75,8 @@ export class ThemeDetailsComponent implements OnInit, OnChanges {
     backButton(){
         this.location.back();
     }
+
+    getTags(){
+        return this.theme.tags.join(', ');
+    }
 }
