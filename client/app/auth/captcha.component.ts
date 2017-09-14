41820d36d81bc1bdabdb08da8efe653da29c3049
Sherjeel Sikander <sherjeel.337@gmail.com>
2017-09-14 18:00:39 +0200

    fix captcha problem when logging in for the second time without page refresh

--- client/app/auth/captcha.component.ts
+++ client/app/auth/captcha.component.ts
@@ -1,44 +1,32 @@
 export class CaptchaComponent {
-  tag_string = '';
-  sitekey='6Lei-x0UAAAAADAAdccFPc23CIlm1tZmaO0-ICwg';
-  captcha = null;
-
-  constructor(private tag: string) {
+    tag_string = '';
+    sitekey='6Lei-x0UAAAAADAAdccFPc23CIlm1tZmaO0-ICwg';
+    captcha = null;
+    widgetId = null;
+    
+    constructor(private tag: string) {
         this.tag_string = tag;
     }
 
-    render(){
-	var tag = this.tag_string;
-    var sitekey = this.sitekey;
-    if (this.captcha != null){
-        this.captcha.render(document.getElementById(tag),{'sitekey':sitekey});
-    }
-	else if (window['grecaptcha'] == undefined){
-		   window.onload =function(){
-            window['grecaptcha'].render(document.getElementById(tag),{'sitekey':sitekey});
-		   }
-	    }else if (window['grecaptcha'] != null){
-            this.captcha = window['grecaptcha'];
-		  window['grecaptcha'].render(document.getElementById(tag),{'sitekey':sitekey});
-	    }
-    }
+  render() {
+      var tag = this.tag_string;
+      var sitekey = this.sitekey;
+      if (window['grecaptcha'] == undefined) {
+          var that = this;
+          window.onload = function () {
+                that.widgetId = window['grecaptcha'].render(document.getElementById(tag), { 'sitekey': sitekey });
+          }
+      }
+      else if (window['grecaptcha'] != null) {
+          this.widgetId = window['grecaptcha'].render(document.getElementById(tag), { 'sitekey': sitekey });
+      }
+  }
 
     getResponse(): string {
-        if (this.captcha != null){
-            return this.captcha.getResponse();
-        }else{
-            this.captcha = window['grecaptcha'];
-            return window['grecaptcha'].getResponse();
-        }
-	    
+        return window['grecaptcha'].getResponse(this.widgetId);
     }
     
     reset(){
-        if (this.captcha != null){
-            this.captcha.reset();
-        }else{
-            this.captcha = window['grecaptcha'];
-            window['grecaptcha'].reset();
-        }
+        window['grecaptcha'].reset(this.widgetId);
     }
 }
