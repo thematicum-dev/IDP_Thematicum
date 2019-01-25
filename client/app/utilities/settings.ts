43b30717dffb64d09295d2e695f93629c0b9b6a1
DESKTOP-GNMDPE6\Valeh <valeh.sabziyev@tum.de>
2019-01-25 18:35:40 +0400

    ref

--- client/app/utilities/settings.ts
+++ client/app/utilities/settings.ts
@@ -1,7 +1,7 @@
 let isDev: boolean = false;
 export function getBaseApi(): string {
     if (isDev) {
-        return 'http://localhost:3000/api/';
+        // return 'http://localhost:3000/api/';
     } else {
        return 'https://thematicum.herokuapp.com/api/';
     }
