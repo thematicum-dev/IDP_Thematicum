087d639bf9a568cc62bd1cdf6bbea9d51915b4a6
DESKTOP-GNMDPE6\Valeh <valeh.sabziyev@tum.de>
2019-04-09 14:45:57 +0200

    cleaning serive works sequentially now

--- server/controllers/removeObsoleteURLs.controller.js
+++ server/controllers/removeObsoleteURLs.controller.js
@@ -11,57 +11,51 @@ let urlExists = require('url-exists-deep');
 export function removeObsoleteURLs(req, res, next) {
 
     repo.getAllReports()
-        .then((reports) => {
-
-            let d = new Date();
-            d.setMonth(d.getMonth() - 12);
-
-            for (let i=0; i<reports.length; i++) {
-                // check if DB entry was created more than 12 months ago
-                if (reports[i]['_id'].getTimestamp() < d) {
-                    repo.removeReportById(reports[i]['_id']);
-                } else {
-                    urlExists(reports[i]['link'], undefined, 'GET', 300000)
-                        .then(function(response){
-                            if (response) {
-                                console.log("Url exists", response.href);
-                            } else {
-                                console.log("Url does not exists!");
-                                console.log(response);
-                                repo.removeReportById(reports[i]['_id']);
-                            }
-                        })
-                        .catch((err) => {
-                            console.log(err);
-                        })
-                }
-            }
-        })
-        .then(() => {
-            repo.getAllNews()
-                .then((news) => {
-                    for (let i=0; i<news.length; i++) {
-                        urlExists(news[i]['url'], undefined, 'GET', 300000)
-                            .then(function(response){
-                                if (response) {
-                                    console.log("Url exists", response.href);
-                                } else {
-                                    console.log("Url does not exists!", response.href);
-                                    console.log(news[i]['url']);
-                                    repo.removeNewsById(news[i]['_id']);
-                                }
-                            })
-                            .catch((err) => {
-                                console.log(err);
-                            })
-                    }
-                });
-        });
-
-
+        .then((reports) => reports.reduce((previous, current) => previous.then(checkReport(current)), Promise.resolve()))
+        .then(() => repo.getAllNews())
+        .then((news) => news.reduce((previous, current) => previous.then(checkNews(current)), Promise.resolve()));
 
     return res.status(200).json(new AppResponse("OK"));
+}
 
+function checkNews(news) {
+    urlExists(news['url'], undefined, 'GET', 300000)
+        .then(function(response){
+            if (response) {
+                console.log("Url exists", response.href);
+            } else {
+                console.log("Url does not exists!", response.href);
+                console.log(news['url']);
+                repo.removeNewsById(news['_id']);
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+}
+
+function checkReport(report) {
+    let d = new Date();
+    d.setMonth(d.getMonth() - 12);
+
+    // check if DB entry was created more than 12 months ago
+    if (report['_id'].getTimestamp() < d) {
+        repo.removeReportById(report['_id']);
+    } else {
+        urlExists(report['link'], undefined, 'GET', 300000)
+            .then(function(response){
+                if (response) {
+                    console.log("Url exists", response.href);
+                } else {
+                    console.log("Url does not exists!");
+                    console.log(response);
+                    repo.removeReportById(report['_id']);
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
 }
 
 
